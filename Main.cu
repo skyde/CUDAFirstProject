101d2880b37e35f0a18e5f482195244dffb451d9
Sky <skydebreuil@gmail.com>
2016-04-21 21:47:42 +0900

    cleanup

--- Main.cu
+++ Main.cu
@@ -50,11 +50,7 @@ __global__ void BackwardPass(
 
 int main(int argc, char **argv)
 {
-//	printf ("N = %d \n", N);
-
 	NeuralNetwork* n = new NeuralNetwork();
-
-//	network->
 
 	n->layers[0]->HostData[0].Self.Value = 1;
 	n->layers[0]->HostData[1].Self.Value = 2;
@@ -67,12 +63,7 @@ int main(int argc, char **argv)
 	n->weights[0]->HostData[2].Value = 0;
 	n->weights[0]->HostData[3].Value = 1;
 
-//	cout << "Generated random values\n";
 	n->CopyToDevice();
-
-//	layers[0]->CopyToDevice();
-//	weights[0]->CopyToDevice();
-//	layers[1]->CopyToDevice();
 
 	cout << "Copy to device calls after initiated\n";
 
@@ -90,16 +81,10 @@ int main(int argc, char **argv)
 			n->weights[0]->DeviceData,
 			n->layers[1]->DeviceData,
 			n->layers[1]->Length);
-//	n->Forward();
-//	n->Backward();
 
 	cout << "RunPass initiated\n";
-//    cudaDeviceSynchronize();
+
 	n->CopyToHost();
-//
-//	layers[0]->CopyToHost();
-//	weights[0]->CopyToHost();
-//	layers[1]->CopyToHost();
 
 	cout << "CopyToHost initiated\n";
 
@@ -115,10 +100,6 @@ int main(int argc, char **argv)
     cout << "print finished\n";
 
     delete n;
-
-//	delete layers[1];
-//	delete weight0to1;
-//	delete layer1;
 
     cout << "dispose finished\n";
 
